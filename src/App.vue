5d752467d3f14228d4b444ee6dc72150285ef944
Roger Steinbakk <rogersteinbakk@gmail.com>
2021-10-06 14:51:01 +0200

    image-slider-vite

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+
+
+<template>
+  <div class="swiper-outer-container">
+    <div class="swiper-size">
+      <ImageSlider :products="products"/>
+    </div>
+  </div>
+</template>
+<script>
+import ImageSlider from "./components/ImageSlider.vue";
+export default {
+  components: {
+    ImageSlider,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    getImages() {
+      fetch("https://fakestoreapi.com/products?limit=5")
+        .then((res) => res.json())
+        .then((data) => (this.products = data));
+    },
+  },
+  created() {
+    this.getImages();
+  },
+};
+</script>
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 50px;
+}
+.swiper-outer-container {
+  display: flex;
+  justify-content: center;
+}
+.swiper-size {
+  max-width: 310px;
+  max-height: 350px;
+  border: 3px solid black;
+}
+</style>
---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,83 @@
+
+<template>
+  <swiper
+    :navigation="true"
+    class="mySwiper"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide v-for="(product, index) in products" :key="index">
+      <img :src="product.image" alt />
+    </swiper-slide>
+    <!-- <swiper-slide>Slide 1</swiper-slide><swiper-slide>Slide 2</swiper-slide
+    ><swiper-slide>Slide 3</swiper-slide><swiper-slide>Slide 4</swiper-slide
+    ><swiper-slide>Slide 5</swiper-slide><swiper-slide>Slide 6</swiper-slide
+    ><swiper-slide>Slide 7</swiper-slide><swiper-slide>Slide 8</swiper-slide
+    ><swiper-slide>Slide 9</swiper-slide>-->
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/navigation";
+
+// import Swiper core and required modules
+import SwiperCore, { Navigation } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+
+export default {
+
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  props: {
+    products: Array
+  },
+  data() {
+    return {
+      // products: [],
+    };
+  },
+
+};
+</script>
+<style scoped>
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: rgb(185, 185, 185);
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
